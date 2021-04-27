957df64a8a3797677aa6873503a796a12749b2b5
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-27 11:03:11 -0400

    init commit

---
+++ SwiftUIResturantSearch/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  SwiftUIResturantSearch
+//
+//  Created by Matthew Garlington on 4/27/21.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ SwiftUIResturantSearch/LoginViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewModel.swift
+//  SwiftUIResturantSearch
+//
+//  Created by Matthew Garlington on 4/27/21.
+//
+
+import SwiftUI
+
+struct LoginViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginViewModel()
+    }
+}
