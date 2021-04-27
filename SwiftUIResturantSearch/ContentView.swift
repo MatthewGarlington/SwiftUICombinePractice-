472ec9b44d21714ea21f26c57186bbe248df4f5f
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-27 10:49:45 -0400

    Initial Commit

---
+++ SwiftUIResturantSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIResturantSearch
+//
+//  Created by Matthew Garlington on 4/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIResturantSearch/SwiftUIResturantSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIResturantSearchApp.swift
+//  SwiftUIResturantSearch
+//
+//  Created by Matthew Garlington on 4/27/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIResturantSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
